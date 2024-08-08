5a78b93c50f2e28aff1fe0c72fa51b835f2db077
Joseph Hefner <Hefnerjoseph87@gmail.com>
2024-08-07 20:12:51 -0700

    Update sb_odds.R

--- R/sb_odds.R
+++ R/sb_odds.R
@@ -2,6 +2,9 @@ library(dplyr)
 library(oddsapiR)
 library(lubridate)
 
+# Saved to GitHub here: 
+# https://github.com/Josephhero/Super-Bowl-Odds/tree/main
+
 super_bowl_lines <- toa_sports_odds(
   sport_key = 'americanfootball_nfl_super_bowl_winner',  
   regions = 'us', 
@@ -11,8 +14,17 @@ super_bowl_lines <- toa_sports_odds(
 ) |> 
   mutate(week_of_year = lubridate::week(Sys.Date())) |> 
   mutate(pull_date = date(Sys.Date())) |> 
+  mutate(super_bowl_year = year(commence_time) - 1) |> 
   filter(bookmaker == "DraftKings") |> 
-  select(week_of_year, pull_date, sport_title, bookmaker_last_update, 
-         bookmaker, outcomes_name, outcomes_price)
+  select(week_of_year, pull_date, sport_title, 
+         super_bowl_year, 
+         super_bowl_date = commence_time, 
+         bookmaker_last_update, bookmaker, 
+         outcomes_name, outcomes_price)
 
-write_csv(super_bowl_lines, "Data/super_bowl_odds.csv")
+write_csv(super_bowl_lines, 
+          paste0("Data/", 
+                 super_bowl_lines$super_bowl_year[1], 
+                 "_super_bowl_odds.csv"))
+
+
