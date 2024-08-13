8471cbcbd03ea70bbb80d5d02f82daab7bd6cbd9
Joseph Hefner <Hefnerjoseph87@gmail.com>
2024-08-13 12:49:51 -0700

    Update sb_odds.R

--- R/sb_odds.R
+++ R/sb_odds.R
@@ -6,9 +6,11 @@ library(lubridate)
 # https://github.com/Josephhero/Super-Bowl-Odds/tree/main
 
 api_key = Sys.getenv("ODDS_API_KEY")
+api_key2 = Sys.getenv("odds_api_secret_input_env")
 
 print("Next line should be API Key")
 
 print(api_key)
+print(api_key2)
 
 
